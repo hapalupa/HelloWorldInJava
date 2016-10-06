fd87f86a8ac9aa1774e0d99d14ba78cce15edc45
Pawel <pawlosik@hotmail.com>
2016-10-06 15:25:58 +0100

    Test Program extended to allow the input of data and then it return the "Hello" + name. if no input entered the program return "Hello World!"

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,29 @@
 
+import java.util.Scanner;
+
 /**
  * @author Pawel
  */
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String input;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name");
+
+        input = scanner.nextLine();
+
+        if (!input.isEmpty()) {
+            System.out.println("Hello " + input + "!");
+        }
+        else {
+            System.out.println("Hello World!");
+        }
+
+        scanner.close();
+
     }
 
 }
